da521ddf31fb023d9b544b837570756c809f8a68
Tin T. Tran <trantin84@gmail.com>
2024-09-30 08:00:21 +0700

    Create baitap11.c

---
+++ LAB_01/baitap11.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	printf("Ho ten: Le Vi Du");	
+ 	printf("Ma so sinh vien: 424H0000");	
+	printf("Nganh: Ky Thuat Dieu Khien Va Tu Dong Hoa");	
+	printf("So Dien Thoai: 076700000");
+	return 0;
+}
